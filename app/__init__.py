6b02765e248bee72c4004f2db930569b1dac303f
hamida-mstafa <hamidamstafa@gmail.com>
2019-08-09 13:34:29 +0300

    config

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://moringaschoolcom:mids@localhost/hamida'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 
 db = SQLAlchemy(app)
